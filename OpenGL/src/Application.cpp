d8bef088b558b0634a88a1c20bb0eea53b699baf
pavlos.aplakidis <pavlos.aplakidis@scigentech.com>
2026-08-12 00:15:31 +0300

    shader bugfix + model view projection matrices

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -31,7 +31,7 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -53,10 +53,10 @@ int main(void)
 	{
 		// Define data (triangle) for shader
 		float positions[] = {
-			-0.5f, -0.5f, 0.0f, 0.0f,	// 0
-			0.5f, -0.5f, 1.0f, 0.0f,	// 1
-			0.5f, 0.5f,	1.0f, 1.0f,		// 2
-			-0.5f, 0.5f, 0.0f, 1.0f		// 3
+			100.0f, 100.0f, 0.0f, 0.0f,	// 0
+			200.0f, 100.0f, 1.0f, 0.0f,	// 1
+			200.0f, 200.0f,	1.0f, 1.0f,	// 2
+			100.0f, 200.0f, 0.0f, 1.0f	// 3
 		};
 
 		// index buffer: instead of adding all vertices inside positions, add only the unique ones
@@ -73,24 +73,26 @@ int main(void)
 		// vertex array object (specifies layout of vertex buffer, i.e. start at 0 with vertex length = 2)
 		unsigned int vao;
 		GLCall(glGenVertexArrays(1, &vao));
-		GLCall(glBindVertexArray(vao));
-
 		VertexArray va;
 		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+		IndexBuffer ib(indices, 6);
+
+		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+		glm::mat4 mvp = proj * view * model;
+
 		VertexBufferLayout layout;
 		layout.Push<float>(2);
 		layout.Push<float>(2);
 		va.AddBuffer(vb, layout);
 
-		IndexBuffer ib(indices, 6);
-
-		glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
-
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+		shader.SetUniformMat4f("u_MVP", mvp);
 
-		Texture texture("res/textures/mario.png");
+		Texture texture("res/textures/gold-dollar.png");
 		texture.Bind();
 		shader.SetUniform1i("u_Texture", 0);
 
@@ -113,7 +115,6 @@ int main(void)
 
 			shader.Bind();
 			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-			shader.SetUniformMat4f("u_MVP", proj);
 
 			renderer.Draw(va, ib, shader);
 
--- OpenGL/src/Shader.cpp
+++ OpenGL/src/Shader.cpp
@@ -11,7 +11,7 @@ Shader::Shader(const std::string& filepath)
 	: m_FilePath(filepath), m_RendererID(0)
 {
 	ShaderProgramSource source = ParseShader(filepath);
-	m_RendererID = CreateShader(source.FragmentSource, source.FragmentSource);
+	m_RendererID = CreateShader(source.VertexSource, source.FragmentSource);
 }
 
 Shader::~Shader()
