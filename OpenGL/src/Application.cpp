39dd8b03c52e93930db17ad500e6f95ab66e5e7d
pavlos.aplakidis <pavlos.aplakidis@scigentech.com>
2026-08-15 18:58:47 +0300

    imgui + render multiple objects + tests

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -17,6 +17,11 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
+
+#include "tests/TestClearColor.h"
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -49,92 +54,41 @@ int main(void)
 
 	GLCall(std::cout << glGetString(GL_VERSION) << std::endl);
 
-	// create scope to avoid errors with glfwTerminate() due to context (alternatively heap allocate Vertex and Index Buffers)
 	{
-		// Define data (triangle) for shader
-		float positions[] = {
-			100.0f, 100.0f, 0.0f, 0.0f,	// 0
-			200.0f, 100.0f, 1.0f, 0.0f,	// 1
-			200.0f, 200.0f,	1.0f, 1.0f,	// 2
-			100.0f, 200.0f, 0.0f, 1.0f	// 3
-		};
-
-		// index buffer: instead of adding all vertices inside positions, add only the unique ones
-		// and tell OpenGL which ones to draw in which order
-		// draw square => 2 triangles
-		unsigned int indices[] = {
-			0, 1, 2,
-			2, 3, 0,
-		};
-
 		GLCall(glEnable(GL_BLEND));
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-		// vertex array object (specifies layout of vertex buffer, i.e. start at 0 with vertex length = 2)
 		unsigned int vao;
 		GLCall(glGenVertexArrays(1, &vao));
-		VertexArray va;
-		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-		IndexBuffer ib(indices, 6);
-
-		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
-		glm::mat4 mvp = proj * view * model;
-
-		VertexBufferLayout layout;
-		layout.Push<float>(2);
-		layout.Push<float>(2);
-		va.AddBuffer(vb, layout);
-
-		Shader shader("res/shaders/Basic.shader");
-		shader.Bind();
-		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4f("u_MVP", mvp);
-
-		Texture texture("res/textures/gold-dollar.png");
-		texture.Bind();
-		shader.SetUniform1i("u_Texture", 0);
-
-		// unbind everything
-		va.Unbind();
-		shader.Unbind();
-		vb.Unbind();
-		ib.Unbind();
 
 		Renderer renderer;
 
-		float r = 0.0f;
-		float increment = 0.05f;
+		ImGui::CreateContext();
+		ImGui_ImplGlfwGL3_Init(window, true);
+		ImGui::StyleColorsDark();
 
-		/* Loop until the user closes the window */
+		test::TestClearColor test;
+
 		while (!glfwWindowShouldClose(window))
 		{
-			/* Render here */
 			renderer.Clear();
 
-			shader.Bind();
-			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+			test.OnUpdate(0.0f);
+			test.OnRender();
 
-			renderer.Draw(va, ib, shader);
+			ImGui_ImplGlfwGL3_NewFrame();
+			test.OnImGuiRender();
+			ImGui::Render();
+			ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
-			// draw
-			if (r > 1.0f) {
-				increment = -0.05f;
-			}
-			else if (r < 0.0f) {
-				increment = 0.05;
-			}
-			r += increment;
-
-			/* Swap front and back buffers */
 			glfwSwapBuffers(window);
 
-			/* Poll for and process events */
 			glfwPollEvents();
 		}
 	}
 
+	ImGui_ImplGlfwGL3_Shutdown();
+	ImGui::DestroyContext();
 	glfwTerminate();
 	return 0;
 }
